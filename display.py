3dbb4a8ad56d21755cb4818b5be5a718ae0e43b2
suika228 <89564141+suika228@users.noreply.github.com>
2024-08-09 14:08:48 +0000

    refact: スキップの処理

--- display.py
+++ display.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+
 from game import Game
 from board import Board
 
@@ -13,10 +14,10 @@ class Display:
         
         # 画面サイズと色設定
         self.GRID_SIZE = 8
-        self.CELL_SIZE = 50  # 各セルのサイズを50ピクセルに固定
+        self.CELL_SIZE = 50
         self.SCREEN_SIZE = self.CELL_SIZE * self.GRID_SIZE
         self.FONT_SIZE = 24
-        self.MARGIN = self.FONT_SIZE + 10  # 文字表示のための余白
+        self.MARGIN = self.FONT_SIZE + 10
         self.SCREEN_WIDTH = self.SCREEN_SIZE + self.MARGIN
         self.SCREEN_HEIGHT = self.SCREEN_SIZE + self.MARGIN
         self.GREEN = (0, 255, 0)
@@ -34,9 +35,8 @@ class Display:
         for x in range(self.GRID_SIZE):
             for y in range(self.GRID_SIZE):
                 rect = pygame.Rect(x * self.CELL_SIZE + self.MARGIN, y * self.CELL_SIZE + self.MARGIN, self.CELL_SIZE, self.CELL_SIZE)
-                pygame.draw.rect(self.screen, self.BLACK, rect, 1)  # グリッド線の描画
+                pygame.draw.rect(self.screen, self.BLACK, rect, 1)
 
-                # グリッドセルの内容に応じた丸の描画
                 if board[y][x] == 'A':
                     pygame.draw.circle(self.screen, self.LIGHT_GRAY, rect.center, self.CELL_SIZE // 4)
                 elif board[y][x] == 'X':
@@ -45,19 +45,16 @@ class Display:
                     pygame.draw.circle(self.screen, self.BLACK, rect.center, self.CELL_SIZE // 4)
 
     def draw_labels(self):
-        # a-h の文字をボードの上部に描画
         letters = "abcdefgh"
         for i in range(self.GRID_SIZE):
             label = self.font.render(letters[i], True, self.BLACK)
             self.screen.blit(label, (i * self.CELL_SIZE + self.MARGIN + self.CELL_SIZE // 2 - label.get_width() // 2, 5))
 
-        # 1-8 の数字をボードの左側に描画
         for i in range(self.GRID_SIZE):
             label = self.font.render(str(i + 1), True, self.BLACK)
             self.screen.blit(label, (5, i * self.CELL_SIZE + self.MARGIN + self.CELL_SIZE // 2 - label.get_height() // 2))
 
     def get_grid_position(self, pos):
-        """ マウスクリック位置をグリッドの座標に変換する """
         x, y = pos
         grid_x = (x - self.MARGIN) // self.CELL_SIZE
         grid_y = (y - self.MARGIN) // self.CELL_SIZE
@@ -79,26 +76,36 @@ class Display:
 
     def run(self):
         was_skipped_turn = False
+        clock = pygame.time.Clock()
+        skipped_count = 0
         while True:
-            
+            clock.tick(60)
             self.game.switch_player()
             self.board.update_puttable_places(self.game.player)
             
-            self.screen.fill(self.GREEN)  # 背景色を緑に設定
+            self.screen.fill(self.GREEN)
             self.draw_board(self.board.board_())
             self.draw_labels()
             pygame.display.flip()
 
             if not self.board.has_place():
-                if was_skipped_turn:
-                    break
-                was_skipped_turn = True
-                continue
+                skipped_count += 1
+
+            if skipped_count > 1:
+                print('終わり')
+                break
+
+            if skipped_count == 1 and not was_skipped_turn:
+                    was_skipped_turn = True
+                    continue
+            if was_skipped_turn:
+                print('スキップ')
             was_skipped_turn = False
+            
             
             x, y = self.click(self.board.puttable_places)
 
-            print(f"クリックされたグリッドの座標: {x},{y}")  # デバッグ用に表示
+            print(f"{x},{y}")
             self.board.put_stone(x, y, self.game.player)
             self.board.flip_stones(x, y, self.game.player)
             self.draw_board(self.board.board_())
@@ -112,6 +119,3 @@ class Display:
             print('白')
         else:
             print('引き分け')
-            
-
-
